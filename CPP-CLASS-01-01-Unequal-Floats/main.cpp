5ffc78ef53229d046976829b6b706117af116b95
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-01-25 13:39:06 -0500

    Initial Commit

---
+++ CPP-CLASS-01-01-Unequal-Floats/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP-CLASS-01-01-Unequal-Floats
+//
+//  Created by Keith Smith on 1/25/18.
+//  Copyright © 2018 Keith Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
